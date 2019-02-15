9a3e988d4cc3e25e7233ae8cf5cbcb30f4cfd632
Matthew Kenworthy <kenworthy@strw.leidenuniv.nl>
2019-02-15 16:55:48 +0100

    expanding test

--- test_Rings.py
+++ test_Rings.py
@@ -1,4 +1,5 @@
 from exorings3.exorings3 import *
+import matplotlib.pyplot as plt
 
 # generate a Ring object p
 
@@ -9,23 +10,31 @@ p = Ring(radius, tau)
 
 #p = Ring()
 
-# add a segment
-p.addRing([2.5,0.8])
-
-# delete a segment
-p.delRing(4.2)
-
-import matplotlib.pyplot as plt
-
-print(p.segments)
+print('Making a ring system with a power law')
 
 plt.scatter(p.getRadius(),p.getTau())
 (r,t) = p.showRing()
-plt.step(r,t, where='pre', zorder=-10)
+plt.step(r,t, where='pre', zorder=-6)
 plt.xlim(0,20)
 plt.ylim(3,-0.1)
 plt.xlabel('Radius')
 plt.ylabel('Optical Depth')
 plt.title('Ring system')
 
+# add a segment
+p.addRing([2.5,0.8])
+print('Added a ring at 2.5, 0.8')
+plt.scatter(p.getRadius(),p.getTau())
+(r,t) = p.showRing()
+plt.step(r,t, where='pre', zorder=-8)
+
+# delete a ring
+p.delRing(4.2)
+print('Added a ring at 2.5, 0.8')
+plt.scatter(p.getRadius(),p.getTau())
+(r,t) = p.showRing()
+plt.step(r,t, where='pre', zorder=-10)
+
 plt.show()
+
+print(p.segments)
